144f11ac634e942955f7243507d5ad3747741d8d
celaeon <celaeon@gmail.com>
2021-05-01 18:06:01 -0400

    fixed connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Google-Book_search', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Google-Book-Search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
